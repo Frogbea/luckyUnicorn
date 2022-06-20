3d789a651fb04bb1f01bf63d040c0dc91dc4cca9
Em <86989285+Frogbea@users.noreply.github.com>
2022-06-21 09:25:25 +1200

    skeleton of the base code

---
+++ 00_base_v0.py
@@ -0,0 +1,16 @@
+'''
+
+base component for the game lucky unicorn
+    v0 - skeleton setup
+author = E.G
+
+'''
+
+# import libraries below this line *************
+
+
+# function set-up
+
+
+# main *******************************************
+print('hello')
